005a9f5cb54070bf5e0db8bb17c9cf5ddca5d2b9
John Doe <johndoe@email.com>
2023-04-16 17:13:57 +0100

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,43 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#4B6BFB",
+        
+"secondary": "#7B92B2",
+        
+"accent": "#67CBA0",
+        
+"neutral": "#181A2A",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#3ABFF8",
+        
+"success": "#36D399",
+        
+"warning": "#FBBD23",
+        
+"error": "#F87272",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
+
+  plugins: [require("daisyui")],
 }
+
+
+//theme: {
+//  extend: {
+//    backgroundImage: {
+//      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+//      'gradient-conic':
+//        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+//    },
+//  },
+//},
